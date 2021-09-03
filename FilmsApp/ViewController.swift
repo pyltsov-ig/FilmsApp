9a0347b5f4c82871c2d1de547b12174bc1665b8a
IGP <pyltsov@gmail.com>
2021-09-03 11:52:08 +0300

    Initial Commit

---
+++ FilmsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmsApp
+//
+//  Created by Igor Pyltsov on 03.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
