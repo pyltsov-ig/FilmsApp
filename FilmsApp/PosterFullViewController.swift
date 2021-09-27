5eca4bbac38cbad900fd7ac13bb32885bf368357
IGP <pyltsov@gmail.com>
2021-09-27 21:41:28 +0200

    For continue DR

---
+++ FilmsApp/PosterFullViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PosterFullViewController.swift
+//  FilmsApp
+//
+//  Created by Igor Pyltsov on 26.09.2021.
+//
+
+import UIKit
+
+class PosterFullViewController: UIViewController {
+
+    @IBOutlet weak var closeButton: UIButton!
+    
+    @IBOutlet weak var posterFullViewImage: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+}
